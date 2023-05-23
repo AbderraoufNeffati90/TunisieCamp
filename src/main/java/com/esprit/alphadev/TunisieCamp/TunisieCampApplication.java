695ecda9b5602bbd0d4f2ff59a2dd40494c08ad2
AbderraoufNeffati90 <abderraouf.neffati@esprit.tn>
2023-05-23 13:11:42 +0100

    first commit

---
+++ src/main/java/com/esprit/alphadev/TunisieCamp/TunisieCampApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.alphadev.TunisieCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunisieCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunisieCampApplication.class, args);
+	}
+
+}
