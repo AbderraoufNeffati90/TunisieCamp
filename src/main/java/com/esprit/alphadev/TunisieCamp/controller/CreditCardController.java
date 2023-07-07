b07ffede7c3cfd2ec376dac4305d7e1066a33df2
Your Name <you@example.com>
2023-07-07 19:09:53 +0200

    dfgb

---
+++ src/main/java/com/esprit/alphadev/TunisieCamp/controller/CreditCardController.java
@@ -0,0 +1,13 @@
+package com.esprit.alphadev.TunisieCamp.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/credit-card")
+public class CreditCardController {
+}
